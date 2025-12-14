838f0ff7c92c3669ac050d075be7d15f5acae9b8
James Murigi <j.murigi.mungai.com>
2025-12-14 10:27:32 +0300

    created meal-item templates

--- capstone/settings.py
+++ capstone/settings.py
@@ -72,13 +72,17 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   # Database backend
+        'NAME': 'capstone_db',                       # Your database name
+        'USER': 'postgres',                          # Your DB username
+        'PASSWORD': 'moonlight@1818',                 # Your DB password
+        'HOST': 'localhost',                         # Or your DB server address
+        'PORT': '5432',                              # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
